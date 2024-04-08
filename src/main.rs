6aec04673b0bc46a6834bff6a46f62082d122088
cash-i1 <cashignitro@gmail.com>
2024-04-08 21:06:09 +1000

    added basic command matching

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use clap::*;
+use colored::Colorize;
 
 fn main() {
     let matches = Command::new("pedotit")
         .about("The best todo app in the an")
+        .subcommand(
+            Command::new("add")
+                .about("Add a new todo item")
+                .arg(
+                    Arg::new("task")
+                        .help("Name of the task")
+                        .short('t')
+                        .long("task")
+                )
+            )
         .get_matches();
+    
+    
+    let test = matches.subcommand_matches("add").unwrap();
+    println!("Adding task '{}' to the DB", test.get_one::<String>("task").unwrap())
 }
