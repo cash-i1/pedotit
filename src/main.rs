87ac784e6c86c3a821dbf16d0bb8dffae5b37623
cash-i1 <cashignitro@gmail.com>
2024-04-09 08:15:32 +1000

    added json saving and better deletion way

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,12 @@ use clap::*;
 use colored::Colorize;
 use serde::*;
 
+const TASKS_JSON_PATH: &str = "./src/tasks.json";
+
 #[derive(Serialize, Deserialize)]
 struct Task {
     name: String,
     description: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Tasks {
-    tasks: Vec<Task>
 }
 
 fn main() {
@@ -74,25 +71,13 @@ fn main() {
                 .get_one::<String>("task")
                 .expect("you need to parse in atask");
 
+            let tasks = load_tasks();
 
-            let tasks = load_json();
-
-            for task in tasks {
-                if &task.name == name {
-                    print!("{}", "- ".red().bold());
-                    println!("{}", name.yellow().bold());
-
-                    print!("{}", "- ".red().bold());
-                    println!("  {}", &task.description.bright_black().italic());
-                    return;
-                }
+            if let Some(task) = tasks.iter().find(|&task| &task.name == name) {
+                println!("{:?}", task.name);
+            } else {
+                println!("{} task could not be found", "?".yellow().bold())
             }
-
-            println!("{} task could not be found", "?".yellow().bold())
-
-
-
-
 
         }
         _ => {
@@ -101,8 +86,13 @@ fn main() {
     }
 }
 
-fn load_json() -> Vec<Task> {
-    let s = fs::read_to_string("./src/tasks.json").expect("couldnt");
+fn load_tasks() -> Vec<Task> {
+    let s = fs::read_to_string(TASKS_JSON_PATH).expect("couldnt");
     let tasks: Vec<Task> = serde_json::from_str(s.as_str()).expect("msg");
     tasks
+}
+
+fn write_tasks(tasks: Vec<Task>) {
+    let tasks: String = serde_json::to_string(&tasks).expect("msg");
+    fs::write(TASKS_JSON_PATH, tasks);
 }
